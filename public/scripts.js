28bf342f998ac836b154b5dc009d2cbb86058115
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-09-25 15:09:19 +0800

    Modified the scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,7 @@
 var myName = "";
 var users = [""];
 $(document).ready(function () {
-    var socket = io.connect('http://localhost:5000');
+    var socket = io();
     socket.emit("request_user", "REQUEST");
     var message = $("#message");
     var username = $("#username");
